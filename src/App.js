a028c6099fa5fcdb4901fcb91db0ea22dd474f57
ivanTabera <ivantabera19@gmail.com>
2020-06-10 15:38:20 -0500

    genere el componente holamundo y lo importamos en el app.js y agregue bootstrap en el proyecto

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import HolaMundo from './HolaMundo'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. <HolaMundo/>
         </p>
         <a
           className="App-link"
---
+++ src/HolaMundo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function HolaMundo(){
+
+    return(
+        <div>Hola mundo</div>
+    )
+}
+
+//Se puede exportar aqui o al declarar la funcion como se muestra 
+/* export default HolaMundo; */
